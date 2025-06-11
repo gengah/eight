651445ddaed5719e7710ed3dcb8fd80fdbc6cc9d
Ogono Genga <ogonogenga@gmail.com>
2025-06-11 22:22:58 +0300

    addigcliengUYhjsOkJghffaHjfFHkjJKms

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ import {
 
 const About = () => {
   return (
-    <div className="bg-white text-black font-sans">
+    <div className="bg-white text-black font-sans pt-24">
       {/* Hero Section */}
       <section className="bg-gray-100 py-20 px-6 md:px-20">
         <div className="max-w-7xl mx-auto flex flex-col-reverse lg:flex-row items-center gap-8">
